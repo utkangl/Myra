9b86e8e94dacd0f229957d66e6e109e04162925f
utkan <utkngl@gmail.com>
2023-09-20 17:26:20 +0300

    horoscope text formatted

--- app/src/main/java/com/example/falci/HoroscopeDetailFragment.kt
+++ app/src/main/java/com/example/falci/HoroscopeDetailFragment.kt
@@ -1,13 +1,16 @@
 package com.example.falci
 
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.falci.internalClasses.dataClasses.GetHoroscopeData
 import com.example.falci.internalClasses.dataClasses.getHoroscopeData
+import com.example.falci.internalClasses.dataClasses.userCompleteProfile
 
 class HoroscopeDetailFragment : Fragment() {
 
@@ -17,12 +20,14 @@ class HoroscopeDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_horoscope_detail, container, false)
-        val getfortune = v.findViewById<ImageView>(R.id.mira_horoscopedetailtop)
         val horoscopeText = v.findViewById<TextView>(R.id.horoscope_textView)
 
-        getfortune.setOnClickListener{
-            horoscopeText.text = getHoroscopeData.toString()
-        }
+
+
+        val horoscope = " Welcome ${userCompleteProfile.name} \n \n Burc Ozetin: \n\n  ${getHoroscopeData.summary}  \n\n Pozitif Yonler: \n \n ${getHoroscopeData.good} \n\n Negatif Yonler: \n\n ${getHoroscopeData.bad}"
+
+
+        horoscopeText.text = horoscope
 
         return v
     }
