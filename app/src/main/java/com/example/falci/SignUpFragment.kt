e997e659f724799a1de50ce9726cfdd0b18fb46a
utkan <utkngl@gmail.com>
2023-09-25 16:44:53 +0300

    taking password input twice and check for match

--- app/src/main/java/com/example/falci/SignUpFragment.kt
+++ app/src/main/java/com/example/falci/SignUpFragment.kt
@@ -24,6 +24,7 @@ class SignUpFragment : Fragment() {
     // them in onCreateView, they would not be re assignable when we came back with back button
     private lateinit var emailField: EditText
     private lateinit var passwordField: EditText
+    private lateinit var passwordAgainField: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +38,7 @@ class SignUpFragment : Fragment() {
 
             emailField = v.findViewById(R.id.signUpFragmentUsernameText)
             passwordField = v.findViewById(R.id.signUpFragmentPasswordText)
+            passwordAgainField = v.findViewById(R.id.signUpFragmentPasswordAgainText)
 
             //creating the json object that will be posted
             val registerJSON = createJsonObject("email" to emailField.text.toString(), "password" to passwordField.text.toString())
@@ -45,29 +47,35 @@ class SignUpFragment : Fragment() {
             userRegister.email = emailField.text.toString()
             userRegister.password = passwordField.text.toString()
 
-            //post registerJson and let user know the error if there is one, else wise toast success
-            postJsonNoHeader(urls.signUpURL, registerJSON, "register") { responseBody, _ ->
+            // if the passwords match each other
+            if (passwordField.text.contentEquals(passwordAgainField.text)){
+                //post registerJson and let user know the error if there is one, else wise toast success
+                postJsonNoHeader(urls.signUpURL, registerJSON, "register") { responseBody, _ ->
 
-                // if response code is 201, toast success and  navigate user to CompleteProfile activity
-                if (statusCode == 201){
-                    println("status code is $statusCode sign up successful")
-                    requireActivity().runOnUiThread {
-                        Toast.makeText(requireContext(), "Basari ile kayit olundu", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(requireActivity(), CompleteProfile::class.java)
-                        startActivity(intent)
+                    // if response code is 201, toast success and  navigate user to CompleteProfile activity
+                    if (statusCode == 201){
+                        println("status code is $statusCode sign up successful")
+                        requireActivity().runOnUiThread {
+                            Toast.makeText(requireContext(), "Basari ile kayit olundu", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(requireActivity(), CompleteProfile::class.java)
+                            startActivity(intent)
+                        }
                     }
-                }
 
-                // if response code is 400, toast the error
-                if (statusCode == 400){
-                    val responseJson = responseBody?.let { it1 -> JSONObject(it1) }
-                    val detail = responseJson?.optString("detail")
-                    requireActivity().runOnUiThread {
-                        Toast.makeText(requireContext(), "$detail", Toast.LENGTH_SHORT).show()
+                    // if response code is 400, toast the error
+                    if (statusCode == 400){
+                        val responseJson = responseBody?.let { it1 -> JSONObject(it1) }
+                        val detail = responseJson?.optString("detail")
+                        requireActivity().runOnUiThread {
+                            Toast.makeText(requireContext(), "$detail", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                }
 
+                }
+            } else{ // if passwords do not match
+                Toast.makeText(requireContext(), "Passwords do not match", Toast.LENGTH_SHORT).show()
             }
+
         }
         return v
     }
